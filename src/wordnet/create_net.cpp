8574e8d2f6dc3b7da7d479f53f4f249054f3630d
Baroxyton <69308275+baroxyton@users.noreply.github.com>
2024-02-06 23:31:16 +0100

    implement network optimization

--- src/wordnet/create_net.cpp
+++ src/wordnet/create_net.cpp
@@ -1,21 +1,22 @@
 #include "create_net.hpp"
-#include <iostream>
 #include "../tokenizers/read_file.hpp"
 #include "../tokenizers/read_tokens.hpp"
 #include "../tokenizers/tokenize.hpp"
 #include "wordnet.hpp"
+#include <algorithm>
+#include <iostream>
+#include <numeric>
 #include <string>
 #include <vector>
-#include <numeric>
 
-void network::normalize_weights(std::vector<double>* weights) {
-    double total = std::accumulate(weights->begin(), weights->end(), 0.0);
+void network::normalize_weights(std::vector<double> *weights) {
+  double total = std::accumulate(weights->begin(), weights->end(), 0.0);
 
-    if (total != 0.0) {
-        for (double& weight : *weights) {
-            weight /= total;
-        }
+  if (total != 0.0) {
+    for (double &weight : *weights) {
+      weight /= total;
     }
+  }
 }
 
 network::Network network::create_net(std::string filename,
@@ -23,7 +24,7 @@ network::Network network::create_net(std::string filename,
 
   if (!tokenizers::file_exists(filename) ||
       !tokenizers::file_exists(token_filename)) {
-      std::cout << "Missing file." << std::endl;
+    std::cout << "Missing file." << std::endl;
     exit(127);
   }
   std::vector<tokenizers::token> tokens =
@@ -33,8 +34,8 @@ network::Network network::create_net(std::string filename,
   std::string current_word;
   for (auto curr_char : wordlist_raw) {
     if (std::string{curr_char} == "\n") {
-      if(current_word.size() != 0){
-      wordlist.push_back(current_word);
+      if (current_word.size() != 0) {
+        wordlist.push_back(current_word);
       }
       current_word = "";
       continue;
@@ -43,55 +44,84 @@ network::Network network::create_net(std::string filename,
   }
 
   Network output;
-  output.layers.push_back(new std::vector<weighted_token*>{
-      new weighted_token{new tokenizers::token{0, "<BEGIN>"}, new std::vector<double>(0)}});
-  
+  output.layers.push_back(new std::vector<weighted_token *>{new weighted_token{
+      new tokenizers::token{0, "<BEGIN>"}, new std::vector<double>(0)}});
+
   for (auto word : wordlist) {
     std::cout << word << std::endl;
     std::vector<tokenizers::token> word_tokens =
         tokenizers::tokenize_string(tokens, word);
 
     while (output.layers.size() < word_tokens.size() + 5) {
-      output.layers.push_back(new std::vector<weighted_token*>(0));
-      for(auto layer : output.layers){
-        if(layer->size() == 0) continue;
+      output.layers.push_back(new std::vector<weighted_token *>(0));
+      for (auto layer : output.layers) {
+        if (layer->size() == 0)
+          continue;
         std::cout << "SIZE: " << layer->size() << (*layer)[0]->token->token;
       }
       std::cout << std::endl;
     }
     int i = 0;
-    word_tokens.push_back(tokenizers::token{1, "<END>"}); 
+    word_tokens.push_back(tokenizers::token{1, "<END>"});
     for (auto word_token : word_tokens) {
-      int last_token = i==0 ? 0 : word_tokens[i-1].id;
-      if(find_layer_tokindex(output.layers[i+1], word_token.id) == -1){
-        try{
-        output.layers[i+1]->push_back(new weighted_token{new tokenizers::token{word_token.id, word_token.token}, new std::vector<double>(output.layers[i+2]->size())});
-        }
-        catch(std::bad_alloc){
-          std::cout << "ERROR_badalloc [" << output.layers[i+2]->size() <<  "]" << std::endl;
-          for(int j = 0; j <  output.layers[i+2]->size(); j++){
-            std::cout << output.layers[i+2]->at(j)->token->token << std::endl;
+      int last_token = i == 0 ? 0 : word_tokens[i - 1].id;
+      if (find_layer_tokindex(output.layers[i + 1], word_token.id) == -1) {
+        try {
+          output.layers[i + 1]->push_back(new weighted_token{
+              new tokenizers::token{word_token.id, word_token.token},
+              new std::vector<double>(output.layers[i + 2]->size())});
+        } catch (std::bad_alloc) {
+          std::cout << "ERROR_badalloc [" << output.layers[i + 2]->size() << "]"
+                    << std::endl;
+          for (int j = 0; j < output.layers[i + 2]->size(); j++) {
+            std::cout << output.layers[i + 2]->at(j)->token->token << std::endl;
           }
         }
-        
-        catch(std::length_error err){
-          std::cout << "ERROR_len [" << std::min<int>(10000, (uint)output.layers[i+2]->size()) << "]" << std::endl;
-        }
 
-        for(weighted_token* weighted_tok : *output.layers[i]){
+        for (weighted_token *weighted_tok : *output.layers[i]) {
           weighted_tok->weigths_out->push_back(0);
         }
-        //output.layers[i][find_layer_tokindex(output.layers[i], last_token)].weigths_out.push_back(0);
+        // output.layers[i][find_layer_tokindex(output.layers[i],
+        // last_token)].weigths_out.push_back(0);
       }
-     output.layers[i]->at(find_layer_tokindex(output.layers[i], last_token))->weigths_out->at(find_layer_tokindex(output.layers[i+1], word_token.id))++;
+      output.layers[i]
+          ->at(find_layer_tokindex(output.layers[i], last_token))
+          ->weigths_out->at(
+              find_layer_tokindex(output.layers[i + 1], word_token.id))++;
       i++;
     }
   }
-  for (size_t i = 0; i < output.layers.size() - 1; ++i) {
-        for (size_t j = 0; j < output.layers[i]->size(); ++j) {
-            normalize_weights(output.layers[i]->at(j)->weigths_out);
-        }
+
+  int min_sample_threshhold = std::min(5, (int)wordlist.size() / 1000);
+  for (int i = 0; i < output.layers.size(); i++) {
+    if (i == output.layers.size() - 1)
+      break;
+    std::vector<int> token_used_total(output.layers[i + 1]->size());
+    for (int j = 0; j < output.layers[i]->size(); j++) {
+      for (int k = 0; k < output.layers[i + 1]->size(); k++) {
+        std::cout << "TEST " << output.layers[i]->at(j)->weigths_out->at(k);
+        token_used_total[k] += (int)output.layers[i]->at(j)->weigths_out->at(k);
+      }
     }
+    for (int j = 0; j < token_used_total.size(); j++) {
+      if (token_used_total[j] < min_sample_threshhold) {
+        //std::cout << "SMALLER THAN THRESHHOLD: " << token_used_total[j] << std::endl;
+        // Remove all connections then remove the token
+        for (weighted_token *tok : *output.layers[i]) {
+          tok->weigths_out->erase(tok->weigths_out->begin() + j);
+        }
+        // Erase the token itself
+        output.layers[i + 1]->erase(output.layers[i + 1]->begin() + j);
+      }
+    }
+  }
+  std::cout << "THRESHHOLD:" << min_sample_threshhold << std::endl;
+
+  for (size_t i = 0; i < output.layers.size() - 1; ++i) {
+    for (size_t j = 0; j < output.layers[i]->size(); ++j) {
+      normalize_weights(output.layers[i]->at(j)->weigths_out);
+    }
+  }
 
   return output;
 };
